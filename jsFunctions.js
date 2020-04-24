e5a1294154ccbd5db66f52c6c0343d51790afb51
sergio9929 <sergio9929@hotmail.com>
2020-04-24 10:19:07 +0200

    añadido texto aleatorio

--- jsFunctions.js
+++ jsFunctions.js
@@ -1,11 +1,48 @@
+function leerJSON() {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "text.json", false);
+    var result={};
+    rawFile.onreadystatechange = () => {
+        if (rawFile.readyState === 4 && rawFile.status === 200 || rawFile.status == 0) {
+            let a = JSON.parse(rawFile.responseText);
+            let s=a.la_llegada_de_los_marcianos["capitulo_" + (Math.round(Math.random() * 17) + 1)]
+            let r= Math.round(Math.random() * s.contenido.length);
+            if(r>s.contenido.length-300){
+                r-=300;
+            }
+            result.titulo=Object.keys(a)[0].replace(/_/g," ")+" - "+s.titulo;
+            let ss1=s.contenido.substring(r, s.contenido.length)
+            let p1= ss1.indexOf(".")
+            let ss2=ss1.substring(p1+2, ss1.length)
+            let p2=ss2.indexOf(".")
+            if (p2+1<150) {
+                let ss3=ss2.substring(p2+2, ss2.length)
+                let p3=ss3.indexOf(".")
+                result.contenido=ss2.substring(0, p2+p3+3)
+                console.log(1)
+            }else{
+                result.contenido=ss2.substring(0, p2+1)
+                console.log(2)
+            }
+        }
+    }
+    rawFile.send(null);
+    return result;
+}
+
 const tpd = "Cuando bajé del avión, el hombre me esperaba con un pedazo de cartón en el que estaba escrito mi nombre. Yo iba a una conferencia de científicos y comentaristas de televisión dedicada a la aparentemente imposible tarea de mejorar la presentación de la ciencia en la televisión comercial. Amablemente, los organizadores me habían enviado un chófer."
 const keystring = document.getElementById("contenteditable");
 var fallos = 0;
 var inicio = 0;
 var primero = true;
-var string1 = tpd.replace(/\s+/g, ' ');
+var texto = leerJSON();
+if (texto.contenido==undefined) {
+    texto.titulo="PREDETERMINADO";
+    texto.contenido=tpd;
+}
 var tema = true;
-document.getElementById("text").innerHTML = string1 + "<div id='linea' class='w-100'></div>";
+document.getElementById("text").innerHTML = texto.contenido + "<div id='linea' class='w-100'></div>";
+document.getElementById("titulo").innerHTML=texto.titulo;
 window.addEventListener("click", () => {
     if (event.target.id != "start") {
         document.getElementById("text").style.backgroundColor = "rgba(0, 0, 0, 0)";
@@ -34,7 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             document.getElementById("text").style.backgroundColor = "rgba(0, 0, 0, .05)"
         }
-        document.getElementById("text").innerHTML = string1 + "<div id='linea' class='w-100'></div>";
+        document.getElementById("text").innerHTML = texto.contenido + "<div id='linea' class='w-100'></div>";
         document.getElementById("text").scroll({
             top: 0,
             left: 0,
@@ -44,11 +81,16 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     document.getElementById("cambiar").addEventListener("click", () => {
         if (document.getElementsByTagName("input")[0].value != "") {
-            string1 = document.getElementsByTagName("input")[0].value.replace(/\s+/g, ' ');
+            texto.contenido = document.getElementsByTagName("input")[0].value.replace(/\s+/g, ' ');
+            document.getElementById("titulo").innerHTML="";
         } else {
-            string1 = tpd.replace(/\s+/g, ' ');
+            texto = leerJSON()
+            if (texto.contenido==undefined) {
+                texto.titulo="PREDETERMINADO";
+                texto.contenido=tpd;
+            }
         }
-        document.getElementById("text").innerHTML = string1 + "<div id='linea' class='w-100'></div>";
+        document.getElementById("text").innerHTML = texto.contenido + "<div id='linea' class='w-100'></div>";
         document.getElementById("text").style.backgroundColor = "rgba(0, 0, 0, 0)";
         document.getElementById("text").scroll({
             top: 0,
@@ -86,7 +128,7 @@ function press() {
         if (e.keyCode == 32) {
             e.preventDefault();
         }
-        if (string1.startsWith(keystring.value)) {
+        if (texto.contenido.startsWith(keystring.value)) {
             if (keystring.value.length > 0) {
                 if (primero) {
                     inicio = Date.now();
@@ -94,13 +136,13 @@ function press() {
                 }
             }
             if (tema) {
-                document.getElementById("text").innerHTML = "<span class='text-primary' style='background-color:rgba(0, 0, 0, .4);'>" + keystring.value.replace(/  +/g, (match) => {return " " + Array(match.length).join('&nbsp;')}) + "</span>" + "<span id='scroll'></span>" + (string1.substring(keystring.value.length, string1.length)) + "<div id='linea' class='w-100'></div>";
+                document.getElementById("text").innerHTML = "<span class='text-primary' style='background-color:rgba(0, 0, 0, .4);'>" + keystring.value.replace(/  +/g, (match) => { return " " + Array(match.length).join('&nbsp;') }) + "</span>" + "<span id='scroll'></span>" + (texto.contenido.substring(keystring.value.length, texto.contenido.length)) + "<div id='linea' class='w-100'></div>";
             } else {
-                document.getElementById("text").innerHTML = "<span class='text-primary' style='background-color:rgba(0, 0, 0, .1);'>" + keystring.value.replace(/  +/g, (match) => {return " " + Array(match.length).join('&nbsp;')}) + "</span>" + "<span id='scroll'></span>" + (string1.substring(keystring.value.length, string1.length)) + "<div id='linea' class='w-100'></div>";
+                document.getElementById("text").innerHTML = "<span class='text-primary' style='background-color:rgba(0, 0, 0, .1);'>" + keystring.value.replace(/  +/g, (match) => { return " " + Array(match.length).join('&nbsp;') }) + "</span>" + "<span id='scroll'></span>" + (texto.contenido.substring(keystring.value.length, texto.contenido.length)) + "<div id='linea' class='w-100'></div>";
             }
             document.getElementById("ppm").innerHTML = Math.round((keystring.value.split(" ").length / ((Date.now() - inicio) / 1000 / 60)) * 100) / 100 + " ppm";
-            
-            if (keystring.value.length == string1.length) {
+
+            if (keystring.value.length == texto.contenido.length) {
                 document.getElementById("text").style.backgroundColor = "rgba(0, 0, 0, 0)";
                 inicio = 0;
                 keystring.removeEventListener("keyup", press);
@@ -108,9 +150,9 @@ function press() {
             }
         } else {
             if (tema) {
-                document.getElementById("text").innerHTML = "<span class='text-danger' style='background-color:rgba(0, 0, 0, .4);'>" + keystring.value.replace(/  +/g, (match) => {return " " + Array(match.length).join('&nbsp;')}) + "</span>" + "<span id='scroll'></span>" + (string1.substring(keystring.value.length, string1.length)) + "<div id='linea' class='w-100'></div>";
+                document.getElementById("text").innerHTML = "<span class='text-danger' style='background-color:rgba(0, 0, 0, .4);'>" + keystring.value.replace(/  +/g, (match) => { return " " + Array(match.length).join('&nbsp;') }) + "</span>" + "<span id='scroll'></span>" + (texto.contenido.substring(keystring.value.length, texto.contenido.length)) + "<div id='linea' class='w-100'></div>";
             } else {
-                document.getElementById("text").innerHTML = "<span class='text-danger' style='background-color:rgba(0, 0, 0, .1);'>" + keystring.value.replace(/  +/g, (match) => {return " " + Array(match.length).join('&nbsp;')}) + "</span>" + "<span id='scroll'></span>" + (string1.substring(keystring.value.length, string1.length)) + "<div id='linea' class='w-100'></div>";
+                document.getElementById("text").innerHTML = "<span class='text-danger' style='background-color:rgba(0, 0, 0, .1);'>" + keystring.value.replace(/  +/g, (match) => { return " " + Array(match.length).join('&nbsp;') }) + "</span>" + "<span id='scroll'></span>" + (texto.contenido.substring(keystring.value.length, texto.contenido.length)) + "<div id='linea' class='w-100'></div>";
             }
             if (e.key != "Backspace") {
                 if (primero == false) {
